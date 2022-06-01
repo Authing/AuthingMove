6ecbe1e28edd5cad7426188f59a1ed0d1c5c9f03
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-06-01 17:04:53 +0800

    feat: adapt qa_ux request response

--- packages/api-proxy/src/apis/network/request.qa_ux.js
+++ packages/api-proxy/src/apis/network/request.qa_ux.js
@@ -4,6 +4,8 @@ import { adaptOptions } from '../../utils'
 export function request (options = {}) {
   const _options = adaptOptions(options, {
     dataType: 'responseType'
+  }, {
+    responseType: options.dataType || 'json'
   })
   return fetch.fetch(_options)
 }
